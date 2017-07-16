3122b87365daeb9ea91700fad9c2c906a8cb51bc
zexxonn <zexxonn@gmail.com>
2017-07-16 19:00:49 +0300

    Prepare Procfile for deployment on Herocy.

--- dbCreate.py
+++ dbCreate.py
@@ -8,12 +8,14 @@ from app import db
 import os.path
 
 db.create_all()
-print("Database created.")
+'''
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     pass
-    #api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    #api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI,
                         SQLALCHEMY_MIGRATE_REPO,
                         api.version(SQLALCHEMY_MIGRATE_REPO))
+'''
+print("Database created.")
